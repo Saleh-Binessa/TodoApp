71744a932fc38d87e3724766c796c3cd18354a08
Saleh-Binessa <saleh.binessa@gmail.com>
2024-04-29 15:52:45 +0300

    Add project files.

---
+++ TodoApp/Migrations/20240429111351_todoDb.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TodoApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class todoDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsCompleted = table.Column<bool>(type: "bit", nullable: false),
+                    DueDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Items",
+                columns: new[] { "Id", "Description", "DueDate", "IsCompleted" },
+                values: new object[,]
+                {
+                    { 1, "Complete all Coded Assignments", new DateTime(2024, 4, 30, 0, 0, 0, 0, DateTimeKind.Unspecified), false },
+                    { 2, "Apartment Rent", null, true },
+                    { 3, "Clean My Room", null, false }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Items");
+        }
+    }
+}
---
+++ TodoApp/Models/TodoContext.cs
@@ -0,0 +1,24 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using TodoApp.Models.Entities;
+
+namespace TodoApp.Models
+
+{
+    public class TodoContext : DbContext
+    {
+        public DbSet<TodoItem> Items { get; set; }
+
+        public TodoContext(DbContextOptions<TodoContext> options)
+           : base(options)
+        { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TodoItem>().HasData(new List<TodoItem>{
+            new TodoItem{ Description = "Complete all Coded Assignments", IsCompleted = false, Id = 1, DueDate = new DateTime(2024,04,30)},
+            new TodoItem{ Description = "Apartment Rent", IsCompleted = true, Id = 2 },
+            new TodoItem{ Description = "Clean My Room", IsCompleted = false, Id = 3 }
+        });
+        }
+    }
+}
